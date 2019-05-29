19e494503ef2764bcade8f598e670e9817364421
Anselmo Borges <anselmoborges@gmail.com>
2019-05-29 19:40:52 -0300

    tudo

--- bin/0-SSLProps.sh
+++ bin/0-SSLProps.sh
@@ -9,4 +9,4 @@ ORGNAME='SPC'
 ORGUNIT='SPCLAB'
 EMAIL=root@${DOMAIN}
 
-HOSTS=sp-vmw-hdp-dev-01.spcdc.br sp-vmw-hdp-dev-02.spcdc.br sp-vmw-hdp-dev-03.spcdc.br sp-vmw-hdp-dev-04.spcdc.br sp-vmw-hdp-dev-05.spcdc.br sp-vmw-hdp-dev-06.spcdc.br sp-vmw-hdp-dev-07.spcdc.br sp-vmw-hdp-dev-08.spcdc.br
+HOSTS=`hostname`
